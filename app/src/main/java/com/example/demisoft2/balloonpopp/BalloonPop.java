7dc231b31727533031b560c7261736aba7dbc22e
Magda <magda.roca15@gmail.com>
2017-03-25 01:31:47 +0200

    fullscreen

---
+++ app/src/main/java/com/example/demisoft2/balloonpopp/BalloonPop.java
@@ -0,0 +1,41 @@
+package com.example.demisoft2.balloonpopp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BalloonPop extends AppCompatActivity {
+
+    private ViewGroup mContentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balloon_pop);
+
+        mContentView = (ViewGroup) findViewById(R.id.activity_main);
+
+        mContentView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                setToFullscreen();
+            }
+        });
+    }
+
+    private void setToFullscreen(){
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setToFullscreen();
+    }
+}
